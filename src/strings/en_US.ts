b87ac28226f580968252c7c6ecc755b0e5dd7c44
Fred Souza <fredericoamsouza@gmail.com>
2016-12-15 13:30:53 -0200

    Filling 'About' page

--- src/strings/en_US.ts
+++ src/strings/en_US.ts
@@ -1,3 +1,5 @@
+export const APP_NAME: string = 'Mangiare';
+
 export const PAGE_CARDS_TITLE: string = 'Who is hanging out';
 export const TAB_CARDS: string = 'People';
 
@@ -19,12 +21,32 @@ export const PAGE_SETTINGS_TERMS: string = 'Use terms';
 
 export const PAGE_ABOUT_TITLE: string = 'About the app';
 export const PAGE_ABOUT_CONTENT: string = `
-    <h1>Here comes the content</h1>
+    <h2>What is it?</h2>
+    <p>
+        ${APP_NAME} is the perfect tool for you to meet new people with the same interests as you to hang
+        out with dinnerds, lunches or event those moments when you only want to try a new cafe, restaurant
+        or bar with someone new.
+    </p>
+    <p>
+        Through the app you can name your wishes for some special plate, be it for having or cooking, and it
+        makes your match with people whom you could share this moment.
+    </p>
+
+    <h2>How it works?</h2>
+    
+    <p>
+        When you open the app you will see a screen showing a stack of cards showing people and what they want
+        to do. Drag the card to left to like and to right to dislike, or just click in the buttons.
+    </p>
+    <p>
+        When you and someone else's likes each other, you will be notified to start talking to this person through
+        a chat, so you can settle the date. Then you just have fun!
+    </p>
 `;
 
 export const PAGE_TERMS_TITLE: string = 'Use Terms';
 export const PAGE_TERMS_CONTENT: string = `
-    <h1>Aqui vai o conteúdo</h1>
+    <h1>Here goes the content</h1>
 `;
 
 export const PAGE_PROFILE_TITLE: string = 'Profile';
--- src/strings/pt_BR.ts
+++ src/strings/pt_BR.ts
@@ -1,3 +1,5 @@
+export const APP_NAME: string = 'Mangiare';
+
 export const PAGE_CARDS_TITLE: string = 'Está querendo sair';
 export const TAB_CARDS: string = 'Pessoas';
 
@@ -19,7 +21,29 @@ export const PAGE_SETTINGS_TERMS: string = 'Termos de uso';
 
 export const PAGE_ABOUT_TITLE: string = 'Sobre o app';
 export const PAGE_ABOUT_CONTENT: string = `
-    <h1>Aqui vai o conteúdo</h1>
+    <h2>O que é?</h2>
+    <p>
+        O ${APP_NAME} é a ferramenta perfeita para você conhecer pessoas com os mesmos interesses que você
+        para jantares, almoços ou até momentos em que apenas desejem conhecer um restaurante diferente com
+        alguém novo.
+    </p>
+    <p>
+        Através do aplicativo você pode indicar a sua vontade de comer um prato em especial ou de conzinhá-lo,
+        caso você seja esse tipo de pessoa, e ele trata de fazer com que você encontre pessoas com quem você
+        pode compartilhar esse momento.
+    </p>
+
+    <h2>Como funciona?</h2>
+
+    <p>
+        Ao abrir o aplicativo, você verá uma tela que exibe uma pilha de cartões mostrando pessoas e o que estão
+        buscando fazer. Arraste para a esquerda para demonstrar interesse ou para a direita para mostrar o 
+        contrário.
+    </p>
+    <p>
+        Quando alguém demonstrar interesse em você e vice-versa, você será notificado para que comece a conversar
+        com a pessoa por chat, para que possam combinar o encontro. Depois disso é só se divertir.
+    </p>
 `;
 
 export const PAGE_TERMS_TITLE: string = 'Termos de uso';
